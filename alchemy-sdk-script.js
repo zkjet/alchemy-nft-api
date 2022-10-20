4c58901a1ec43214ceb0c8be4e7382faa9090586
zKjet <100480520+zkjet@users.noreply.github.com>
2022-10-20 15:01:34 -0600

    initial commit

---
+++ alchemy-sdk-script.js
@@ -0,0 +1,46 @@
+// This script demonstrates access to the NFT API via the Alchemy SDK.
+import { Network, Alchemy } from "alchemy-sdk";
+
+// Optional Config object, but defaults to demo api-key and eth-mainnet.
+const settings = {
+  apiKey: "API_KEY", // Replace with your Alchemy API Key.
+  network: Network.ETH_MAINNET, // Replace with your network.
+};
+
+const alchemy = new Alchemy(settings);
+
+// Print owner's wallet address:
+const ownerAddr = "0xshah.eth";
+console.log("fetching NFTs for address:", ownerAddr);
+console.log("...");
+
+// Print total NFT count returned in the response:
+const nftsForOwner = await alchemy.nft.getNftsForOwner("0xshah.eth");
+console.log("number of NFTs found:", nftsForOwner.totalCount);
+console.log("...");
+
+// Print contract address and tokenId for each NFT:
+for (const nft of nftsForOwner.ownedNfts) {
+  console.log("===");
+  console.log("contract address:", nft.contract.address);
+  console.log("token ID:", nft.tokenId);
+}
+console.log("===");
+
+// Fetch metadata for a particular NFT:
+console.log("fetching metadata for a Crypto Coven NFT...");
+const response = await alchemy.nft.getNftMetadata(
+  "0x5180db8F5c931aaE63c74266b211F580155ecac8",
+  "1590"
+);
+
+// Uncomment this line to see the full api response:
+// console.log(response);
+
+// Print some commonly used fields:
+console.log("NFT name: ", response.title);
+console.log("token type: ", response.tokenType);
+console.log("tokenUri: ", response.tokenUri.gateway);
+console.log("image url: ", response.rawMetadata.image);
+console.log("time last updated: ", response.timeLastUpdated);
+console.log("===");
