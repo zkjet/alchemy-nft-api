a402ebd05fb7abbe71cef4a0ca02148f4303dcc5
zKjet <100480520+zkjet@users.noreply.github.com>
2022-10-20 15:08:02 -0600

    added alchemy-sdk

--- alchemy-sdk-script.js
+++ alchemy-sdk-script.js
@@ -4,7 +4,7 @@ import { Network, Alchemy } from "alchemy-sdk";
 // Optional Config object, but defaults to demo api-key and eth-mainnet.
 const settings = {
   apiKey: "API_KEY", // Replace with your Alchemy API Key.
-  network: Network.ETH_MAINNET, // Replace with your network.
+  network: Network.ETH_GOERLI, // Replace with your network.
 };
 
 const alchemy = new Alchemy(settings);
